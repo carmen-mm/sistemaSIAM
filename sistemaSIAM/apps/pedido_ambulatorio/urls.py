9e8cd3a5a923c8a4d202467e4ff2ed93fd9cefa4
carmen-mm <carmela_mm@hotmail.com.ar>
2020-09-12 18:47:31 -0300

    Modificar Formulario inline de Pedidos ambulatorios

--- sistemaSIAM/apps/pedido_ambulatorio/urls.py
+++ sistemaSIAM/apps/pedido_ambulatorio/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from apps.pedido_ambulatorio.views import index, DiagnosticoList, Detalle_Pedido
+from apps.pedido_ambulatorio.views import index, DiagnosticoList, Detalle_Pedido, PedidosListar, Opciones, PedidoModificar
 
 # en urlpatterns se listan todas las urls de las vistas de la app pedido_ambulatorio
 app_name = "practicasMedicas"
@@ -8,6 +8,10 @@ app_name = "practicasMedicas"
 urlpatterns = [
     path('', login_required(index), name='index'), #el segundo parámetro que recibe path(,) es la vista de mi apps pedido_ambulatorio
     path('listarD', login_required(DiagnosticoList.as_view())), #el tercer parámetro es el nombre de la URL para identificar a la vista
-    path('detalle', Detalle_Pedido, name='detalle')
+    path('detalle', Detalle_Pedido, name='detalle'),
+    path('listarP', PedidosListar.as_view(), name='listarP'),
+    path('modificar/<int:pk>', PedidoModificar.as_view(), name='modificar'),
+    path('opciones', Opciones, name='opciones')
+
 ]
 
--- sistemaSIAM/apps/pedido_ambulatorio/views.py
+++ sistemaSIAM/apps/pedido_ambulatorio/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.forms import inlineformset_factory
 from apps.pedido_ambulatorio.forms import PedidoAmbulatorioForm, DetallesForm
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 from django.urls import reverse_lazy
 from apps.pedido_ambulatorio.models import Diagnostico, Detalle_PedidoMedico, Pedido_Ambulatorio
 
 
+def Opciones(request):
+  return render(request,'practicasMedicas/opciones.html')
 
 def index(request):
   return render(request, 'practicasMedicas/index.html')
@@ -23,26 +25,56 @@ def Detalle_Pedido(request):
         Pedido_Ambulatorio,
         Detalle_PedidoMedico,
         form=DetallesForm,
-        # fields=('autorizado',
-        #         'importeCoseguro',
-        #         'observaciones',
-        #         'pedido',
-        #         'doctores',
-        #         'practicas',),
         extra=2
     )
     form = PedidoAmbulatorioForm(request.POST or None)
     formset = ItemFormSet(request.POST or None, instance=form.instance)
     if request.method == 'POST':
-        # form = PedidoAmbulatorioForm(request.POST)
-        # formset = ItemFormSet(request.POST)
         if form.is_valid() and formset.is_valid():
             paf = form.save(commit=False)
             paf.user = request.user
             paf.save()
             formset.save()
-            # form.save()
-            # formset.save()
-            url = reverse_lazy('centromedico:listarCM')
+            url = reverse_lazy('practicasMedicas:listarP')
             return HttpResponseRedirect(url)
     return render(request, 'practicasMedicas/detallePedido.html', {'form': form, 'formset': formset })
+
+class PedidosListar(ListView):
+    model = Pedido_Ambulatorio
+    template_name = 'practicasMedicas/listar_pedidos.html'
+    paginate_by = 15
+
+class PedidoModificar(UpdateView):
+    model = Pedido_Ambulatorio
+    form_class = PedidoAmbulatorioForm
+    template_name = 'practicasMedicas/detallePedido.html'
+
+    def get_success_url(self):
+        self.success_url = reverse_lazy('practicasMedicas:listarP')
+        return self.success_url
+
+    def get_context_data(self, **kwargs):
+        context = super(PedidoModificar, self).get_context_data(**kwargs)
+        ItemFormSet = inlineformset_factory(
+            Pedido_Ambulatorio,
+            Detalle_PedidoMedico,
+            form=DetallesForm,
+            extra=2
+        )
+        if self.request.POST:
+            context['formset'] = ItemFormSet(self.request.POST, instance=self.object)
+        else:
+            context['formset'] = ItemFormSet(instance=self.object)
+        return context
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        context = self.get_context_data()
+        formset = context['formset']
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
